00aef25aa5ad4493385c3a0391610ae43e7f1347
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-01-23 20:36:22 -0600

    greeting query done!!

--- hello/src/index.ts
+++ hello/src/index.ts
@@ -1,20 +1,25 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
  
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
   type Query {
-    hello: String
+    greetingPerson(name: String!, age: Int): String
   }
 `);
+
+interface Person {
+  name: string;
+  age: number 
+}
  
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
+    greetingPerson: ( data: Person ) => {
+        return `hello ${data.name} ${data.age}`;
+    }
+  };
  
 const app = express();
 app.use('/graphql', graphqlHTTP({
@@ -22,5 +27,7 @@ app.use('/graphql', graphqlHTTP({
   rootValue: root,
   graphiql: true,
 }));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+app.listen(4000, ()=> {
+  console.log(`🚀  Server ready at http://localhost:4000/graphql`);
+});
