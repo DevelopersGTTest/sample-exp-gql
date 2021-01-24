9fdaf1ddb1815ba612dd2903a0af70993abdcf9e
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-01-23 23:12:10 -0600

    create dynamic person

--- hello/src/index.ts
+++ hello/src/index.ts
@@ -1,25 +1,42 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
- 
+import { Person } from './person.model';
+
+
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type Query {
-    greetingPerson(name: String!, age: Int): String
+  input PersonInput {
+    name: String
+    age: Int
+  }
+
+  type Person {
+    id: ID!
+    name: String
+    age: Int
+  }
+
+  type Query { 
+    getPersonById(id: ID!): Person 
+  }
+
+  type Mutation {
+    createPerson(input: PersonInput): Person
+    updatePerson(id: ID!, input: PersonInput ): Person
   }
 `);
 
-interface Person {
-  name: string;
-  age: number 
-}
  
 // The root provides a resolver function for each API endpoint
 const root = {
-    greetingPerson: ( data: Person ) => {
-        return `hello ${data.name} ${data.age}`;
-    }
-  };
+  createPerson: ( rawData: Person ) => {
+    const person = JSON.parse(JSON.stringify(rawData)).input;
+    // Create a random id for our "database".
+    const id = require('crypto').randomBytes(10).toString('hex');
+    return new Person( id, person.name, person.age );
+  }
+};
  
 const app = express();
 app.use('/graphql', graphqlHTTP({
---
+++ hello/src/person.model.ts
@@ -0,0 +1,10 @@
+export class Person {
+    id = 0;
+    name = '';
+    age = 0;
+    constructor(id:any, name: string, age: number ){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
