1f29f9cbc27e257ff649992275d43f58c31e52c4
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-02-11 17:55:26 -0600

    optimized query books

--- api-gql-with-mysql/src/controllers/book-controller.ts
+++ api-gql-with-mysql/src/controllers/book-controller.ts
@@ -6,16 +6,12 @@ export const bookController = {
             const query = (await DB_CONFIG)
             .query(`
                 SELECT 
-                    b.id_book, b.asin, b.name, 
-                    b.editorial, b.lang, b.cover, 
-                    b.isbn, b.id_category, c.name as 'categoryName' , 
-                    b.id_author, a.name as 'nameAuthor'  
-                FROM 
-                    book b, category c, author a 
-                WHERE 
-                    b.id_category = c.id_category 
-                AND 
-                    b.id_author = a.id_author
+                    book.*,
+                    category.name as 'categoryName',
+                    author.name AS 'nameAuthor'
+                FROM book
+                INNER JOIN category ON book.id_category = category.id_category
+                INNER JOIN author ON book.id_author = author.id_author
             `);
             return query;
         } catch (error) {
