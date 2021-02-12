b9f34bf817db327ccafd3ea52a03ae524e8b4248
p-jacobo2012240 <pablojacobo1998@gmail.com>
2021-02-12 11:37:17 -0600

    add find method in controller

--- api-gql-with-mysql/src/controllers/book-controller.ts
+++ api-gql-with-mysql/src/controllers/book-controller.ts
@@ -35,5 +35,22 @@ export const bookController = {
         } catch (error) {
             console.log(error);
         }
+    },
+    findOneBook:  async(idBook: number) => {
+        try {
+            const query = ( await DB_CONFIG).query(`
+                SELECT 
+                    book.*,
+                    category.name AS 'categoryName',
+                    author.name AS 'nameAuthor'
+                FROM book 
+                INNER JOIN category ON book.id_category = category.id_category
+                INNER JOIN author ON book.id_author = author.id_author
+                WHERE book.id_book = ${idBook}
+            `);
+            return query;
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
